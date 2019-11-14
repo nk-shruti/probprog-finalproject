13ce82383f3411fbaf7f9b7d4fb474daafe0710c
nk-shruti <nks.shruti@gmail.com>
2019-11-14 11:57:12 -0500

    Fixing flake8 issues

--- data_preprocess/add_places_to_data.py
+++ data_preprocess/add_places_to_data.py
@@ -3,29 +3,35 @@ from tqdm import tqdm
 import pandas as pd
 import sys
 
+BASE = 'https://maps.googleapis.com/maps/api/place/nearbysearch'
+STR_REQ = BASE + '/json?location={},{}&radius={}&type={}&key={}'
+
+
 def main(api_key):
-    data = pd.read_csv('data/final_data.csv')
+    data = pd.read_csv('data/block_data.csv')
     latitudes = data['Latitude']
     longitudes = data['Longitude']
     radius = 500
     places = ['restaurants', 'bar', 'park']
-    latitudes  = data['Latitude']
+    latitudes = data['Latitude']
     longitudes = data['Longitude']
-    radius = 500 #meters
+    radius = 500  # meters
     k = {}
     counter = 0
-    with tqdm(total=len(places)*len(latitudes)) as pbar:
+    with tqdm(total=len(places) * len(latitudes)) as pbar:
         for place in places:
             k[place] = []
             for lat, long in zip(latitudes, longitudes):
-                res = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius={}&type={}&key={}'.format(lat, long, radius, place, api_key))
+                req = STR_REQ.format(lat, long, radius, place, api_key)
+                res = requests.get(req)
                 k[place].append(len(res.json()['results']))
-                counter+=1
+                counter += 1
                 pbar.update(counter)
-    
+
     for place in places:
         data[place] = k[place]
     data.to_csv('data/places.csv')
-        
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main(sys.argv[1:])
